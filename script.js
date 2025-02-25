d77697743aa6d3741acff3f64518b8cd6c3d0985
WortheyWebDev <ryan.worthey@live.com>
2025-02-25 12:00:04 -0500

    Finalize core functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const inputField = document.querySelector('.input-field')
 const submitBtn = document.querySelector('.submit-btn')
 const listContainer = document.querySelector('.list-container')
+const questDetailsTextarea = document.querySelector('.quest-details')
+const questTitle = document.querySelector('.quest-title')
 
 let itemArray = []
+let selectedQuestId = null
 
 document.addEventListener('click', function(e) {
     if (e.target.tagName === 'BUTTON') {
@@ -15,7 +18,31 @@ document.addEventListener('click', function(e) {
     else if (e.target.classList.contains('delete-btn')) {
         removeItem(e)
     }
+
+    else if (e.target.closest('.list-item')) {
+        const questId = e.target.closest('.list-item').dataset.id
+        selectedQuestId = questId
+
+        const selectedQuest = itemArray.find((item) => item.id === questId)
+            if (selectedQuest) {
+                questDetailsTextarea.value = selectedQuest.questDetails || ""
+                questTitle.textContent = selectedQuest.item
+            }
+    }
 })
+
+questDetailsTextarea.addEventListener('input', function() {
+    getQuestDetails()
+})
+
+function getQuestDetails() {
+    if (selectedQuestId) {
+        const selectedQuest = itemArray.find((item) => item.id === selectedQuestId)
+        if (selectedQuest) {
+            selectedQuest.questDetails = questDetailsTextarea.value
+        }
+    }
+}
 
 function removeItem(e) {
     const itemId = e.target.dataset.delete
@@ -36,7 +63,8 @@ function getItemInput(e) {
         itemArray.push({
             id: crypto.randomUUID(),
             item: inputField.value,
-            isChecked: false
+            isChecked: false,
+            questDetails: ""
         })
     }
 }
@@ -45,7 +73,7 @@ function renderList() {
     let listItems = ""
     itemArray.forEach((item) => {
         listItems += `
-            <div class="list-item ${item.isChecked ? 'checked' : ''}">
+            <div class="list-item ${item.isChecked ? 'checked' : ''}" data-id="${item.id}">
                 <input type="checkbox" data-checkbox="${item.id}" ${item.isChecked ? 'checked' : ''}>
                 <p>${item.item}</p>
                 <p class="delete-btn" data-delete="${item.id}">X</p>
